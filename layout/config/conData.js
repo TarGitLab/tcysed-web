7c1cb45c03a2f8cc66af28390629a22d95f76dc5
0xtarit <0xtarit@gmail.com>
2025-12-02 21:54:13 +0530

    backend url changed

--- layout/config/conData.js
+++ layout/config/conData.js
@@ -1,4 +1,4 @@
-let apiRequestUrl = "http://127.0.0.1:3000";
+let apiRequestUrl = "https://project1-backend-delta.vercel.app";
 let encPassword = "Tcysed@123";
 
 let telegramOwnerUsername = "https://t.me/evm_recover";
